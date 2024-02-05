9bc9e3bb052ee79d8d685ad454a6ab47c9390daf
KothiyalAkshat99 <kothiyalakshat@ufl.edu>
2024-02-04 23:11:10 -0500

    main.py changes - handling blanks

--- assignment0/main.py
+++ assignment0/main.py
@@ -25,9 +25,35 @@ def extractincidents(incident_data):
 
     for page_number in range(len(doc)):
 
+        global ls
+        ls = []
+        
         x = doc[page_number]
-        text = x.get_text("text")
+        text = x.get_text("blocks") #"text" alternate, keepspaces = True
 
+        if page_number == 0: # Removing extraneous info from first page
+            text.pop(0)
+            text.pop()
+            text.pop()
+        elif page_number == len(doc)-1: # Removing extraneous info from last page
+            text.pop()
+        
+        for t in text: # Splitting text list into required columns
+            #print(type(t), end='\n')
+            temp = t[4].split('\n')
+            temp.remove('')
+            if(len(temp)<5):
+                temp.insert(2, ' ')
+                temp.insert(3, ' ')
+            ls.append(temp)
+
+        dttime.append([sublist[0] for sublist in ls])
+        inc_no.append([sublist[1] for sublist in ls])
+        loc.append([sublist[2] for sublist in ls])
+        nature.append([sublist[3] for sublist in ls])
+        inc_ori.append([sublist[4] for sublist in ls])
+        #print(loc)
+        '''
         if page_number == 0: # Replacing/ Removing Extraneous Info from PDF text extract
             text = text.replace('Date / Time', '')
             text = text.replace('Incident Number', '')
@@ -38,12 +64,14 @@ def extractincidents(incident_data):
             text = text.replace('Daily Incident Summary (Public)', '')
     
         text = text.strip() # Trimming extra spaces
+        print(text)
         tsplit = text.split('\n') # Splitting text into rows
 
         #page_number = 22
         if page_number == len(doc)-1: # Removing extraneous info from last page of PDF
             tsplit.pop(len(tsplit)-1)
-        
+        #for t in tsplit:
+            #print(t, end='\n')
         #print(type(tsplit))
         dttime.append(tsplit[0::5]) # Splicing for date and time
         inc_no.append(tsplit[1::5]) # Splicing for incident number
@@ -51,6 +79,7 @@ def extractincidents(incident_data):
         nature.append(tsplit[3::5]) # Splicing for nature
         inc_ori.append(tsplit[4::5]) # Splicing for incident ORI
         #print(dttime)
+        '''
 
     incidents = [dttime, inc_no, loc, nature, inc_ori]
     return incidents
@@ -58,7 +87,7 @@ def extractincidents(incident_data):
 
 def createdb():
 
-    con = sqlite3.connect("resources/normanpd.db") # Creating Database connection
+    con = sqlite3.connect("normanpd.db") # Creating Database connection
     cur = con.cursor() # Database Cursor
 
     #cur.execute("DROP TABLE incidents;")
@@ -104,8 +133,18 @@ def status(db):
     cur = db.cursor()
     
     res = cur.execute("SELECT nature, COUNT(*) FROM incidents GROUP BY nature ORDER BY COUNT(*) DESC, nature ASC")
+    
+    global bl
+    bl = []
+
     for t in res.fetchall():
+        if t[0]==' ':
+            bl.append(t[1])
+            continue
         print(f'{t[0]} | {t[1]}', end='\n')
+    
+    for t in bl:
+        print(f'  | {t}', end='\n')
     
     db.close()
 
